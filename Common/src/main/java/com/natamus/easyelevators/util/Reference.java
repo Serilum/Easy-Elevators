60b0eeeef6793f7ffdd4d2bd04288a402703e828
Rick South <rick@natamus.com>
2024-08-09 08:26:27 +0200

    Easy Elevators source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/easyelevators/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.easyelevators.util;
+
+public class Reference {
+	public static final String MOD_ID = "easyelevators";
+	public static final String NAME = "Easy Elevators";
+	public static final String VERSION = "1.1";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
