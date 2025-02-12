57704044b5e3908995a19326067726c2c94b5f79
Rick South <rick@natamus.com>
2025-02-12 23:36:39 +0100

    (v1.3) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/easyelevators/forge/events/ForgeElevatorEvents.java
+++ Forge/src/main/java/com/natamus/easyelevators/forge/events/ForgeElevatorEvents.java
@@ -5,12 +5,10 @@ import com.natamus.easyelevators.util.Util;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeElevatorEvents {
     @SubscribeEvent
-    public void onWorldLoad(LevelEvent.Load e) {
+    public static void onWorldLoad(LevelEvent.Load e) {
         Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
         if (level == null) {
             return;
