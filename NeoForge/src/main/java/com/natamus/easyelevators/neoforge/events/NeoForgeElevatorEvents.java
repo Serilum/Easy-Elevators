6ebaf12ea035c75a4e3350f14eb7d77b32c9f5f5
Rick South <rick@natamus.com>
2025-02-12 23:36:43 +0100

    (v1.3) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/easyelevators/neoforge/events/NeoForgeElevatorEvents.java
+++ NeoForge/src/main/java/com/natamus/easyelevators/neoforge/events/NeoForgeElevatorEvents.java
@@ -4,10 +4,8 @@ import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.easyelevators.util.Util;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.LevelEvent;
 
-@EventBusSubscriber
 public class NeoForgeElevatorEvents {
 	@SubscribeEvent
 	public static void onWorldLoad(LevelEvent.Load e) {
