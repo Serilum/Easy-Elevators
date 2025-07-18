f1b05c8387758238227d961cec1e913858e2b798
Rick South <rick@natamus.com>
2025-07-18 03:05:16 +0200

    Easy Elevators source code for 1.21.8.

---
+++ NeoForge/src/main/java/com/natamus/easyelevators/neoforge/events/NeoForgeElevatorEvents.java
@@ -0,0 +1,19 @@
+package com.natamus.easyelevators.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.easyelevators.util.Util;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
+
+public class NeoForgeElevatorEvents {
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		Util.processConfigBlocks(level);
+	}
+}
