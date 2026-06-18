7c463063c0fb38cbe28da6d13273d0fed48416f7
Rick South <rick@natamus.com>
2026-06-18 23:59:10 +0200

    Updated Easy Elevators to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/easyelevators/util/Reference.java
+++ Common/src/main/java/com/natamus/easyelevators/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "easyelevators";
 	public static final String NAME = "Easy Elevators";
 	public static final String VERSION = "1.3";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
